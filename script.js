8c6b2b74da07cd17bb5b1676c78edd713498049f
slowdolphin <tighe59@gmail.com>
2021-07-20 18:58:43 -0400

    verbal questions completed

---
+++ script.js
@@ -0,0 +1,10 @@
+// Verbal Questions
+// Write answers to the following questions as comments. As a reminder you can highlight your code and type command+ /to turn your code into a comment.
+//
+// // my commented answer
+
+// What is the difference between a parameter and an argument?
+//parameters are defined within the function, where the arguments are values passed to the function.
+
+// Within a function, what is the difference between return and console.log?
+//console.log() is a function to print information to the console, where the retrun passes a value where the call was made. 
